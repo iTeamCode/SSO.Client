42743a494b30f12914485f34234f1cd4548ecf4d
Alan luo <iteamalan@hotmail.com>
2015-10-13 10:44:37 +0800

    no message

---
+++ WebApp.Dashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp.Dashboard.Startup))]
+namespace WebApp.Dashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
