1b1336347efc9f90ffec2c1b30e180845c6ebae5
Alan luo <iteamalan@hotmail.com>
2015-10-14 14:07:39 +0800

    update

---
+++ WebApp.Teacher/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Teacher.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Demo/Signin
+        public ActionResult Signin()
+        {
+            return View();
+        }
+	}
+}
